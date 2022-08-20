bcc905b696fd367f7ab97268b76131c676c49448
Yifan <datasciencewyfan@gmail.com>
2022-08-20 16:18:36 -0400

    Working on post serializer

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
 
     belongs_to :user
     belongs_to :sports_category
-    belongs_to :drill
+    
+    # belongs_to :drill
     # belongs_to :workout_session
     # belongs_to :workout_program
 
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,21 +1,24 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id , :post_subtitle, :workout_session, :workout_program, :drill_url
+  attributes :id, :post_subtitle, :workout_session, :workout_program, :drill, :workout_session, :workout_program
 
   belongs_to :user
   belongs_to :sports_category
-  belongs_to :drill
+  
+  # belongs_to :drill
   # belongs_to :workout_session
   # belongs_to :workout_program
 
-  def drill_url
-    rails_blob_path(Drill.find(self.object.drill_id).video_data, only_path: true)
+  def drill
+    if self.object.drill_id
+      drill_hash = JSON.parse( Drill.find( self.object.drill_id ).to_json )
+      drill_hash.store( :video_data , rails_blob_path(Drill.find(self.object.drill_id).video_data, only_path: true) )
+      return drill_hash
+    else
+      return nil
+    end
   end
-
-  # def drill
-  #   Drill.find(self.object.drill_id)
-  # end
 
   def workout_session
     nil
