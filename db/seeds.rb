6247448003eefb1854f7fa8513db10b068d34a45
Yifan <datasciencewyfan@gmail.com>
2022-08-24 00:09:57 -0400

    MVP finished, minor change

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ SportsCategory.create(sport_name: "tennis" , sport_image: "https://upload.wikime
 SportsCategory.create(sport_name: "football" , sport_image: "https://upload.wikimedia.org/wikipedia/commons/2/23/Wilson_American_football.jpg")
 SportsCategory.create(sport_name: "golf" , sport_image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Golf_ball_resting_near_fairway_wood.jpg")
 SportsCategory.create(sport_name: "weight lifting" , sport_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Chrome_dumbbells.jpg/2560px-Chrome_dumbbells.jpg")
-SportsCategory.create(sport_name: "skateboard" , sport_image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Skateboard_facility_at_Guantanamo.jpg")
+SportsCategory.create(sport_name: "skateboard" , sport_image: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Rayssa_Leal_durante_a_prova_em_T%C3%B3quio.jpg")
